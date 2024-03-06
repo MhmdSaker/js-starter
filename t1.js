3aa4abaeb7654b88bb5458bdcef6b1c93ea3c55c
Muhammad Saker <81496112+MhmdSaker@users.noreply.github.com>
2024-03-07 01:03:25 +0200

    Create string.js
    playing with concatenation and interpolation (template literal)

---
+++ t1.js
@@ -0,0 +1,14 @@
+var string = `Muhammad`
+
+console.log(`My name is ${string}`)
+console.log("My name is " + string)
+
+console.log("Total cost: " + "$" + (3 + 5))
+console.log(`Total cost: $${3 + 5}`)
+// alert(`Total cost: $${3 + 5}`)
+
+var total = (599 + 295) / 100
+console.log("Total cost: $" + total)
+console.log(`Total cost: $${total}
+Thank you, come again!
+`)
