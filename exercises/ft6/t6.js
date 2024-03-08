77e8b8f1749e9195898ccc38584c256a2975a0c9
MhmdSaker <professional24gamer7@gmail.com>
2024-03-08 02:49:53 +0200

    Playing with if-statements
    bruh moments

---
+++ exercises/ft6/t6.js
@@ -0,0 +1,11 @@
+let hour = 2;
+let name1 = "Muhammad"
+
+
+if(hour > 6 && hour < 12 ) {
+    console.log(`Good Morning! ${name1}`)
+}else if(hour > 13 && hour < 17) {
+    console.log(`Good Afternoon! ${name1}`)
+} else {
+    console.log(`Good Night ${name1}!`);
+}
