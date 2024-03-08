1536e721e4af84364aa076bc3c19452fa792a72a
MhmdSaker <professional24gamer7@gmail.com>
2024-03-08 20:20:41 +0200

    Update t7.js w/ 7d, 7e, 7f

--- exercises/ft7/t7.js
+++ exercises/ft7/t7.js
@@ -20,3 +20,37 @@ greet('');
 greet("Muhammad")
 
 
+// 7d, 7e, 7f
+
+
+function celToFah(cel) {
+    let fahrenheit = (cel * 9/5) + 32
+    return fahrenheit;
+}
+
+
+function fahToCel(fah) {
+    let celsius = (fah - 32) * 5/ 9
+    return celsius;
+}
+
+let val = '';
+
+
+function convertTemperature(deg, unit) {
+
+    if(unit == 'F') {
+        val = fahToCel(deg)
+        return val + 'C';
+    }else if (unit == 'C') {
+        val = celToFah(deg);
+        return val + 'F';
+    }
+}
+
+
+console.log(celToFah(25));
+console.log(fahToCel(86));
+
+console.log(convertTemperature(86, 'F'));
+console.log(convertTemperature(25, 'C'));
