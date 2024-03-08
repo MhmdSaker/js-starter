6752131a871e4eabce05cddeab2542101b14051e
MhmdSaker <professional24gamer7@gmail.com>
2024-03-08 03:53:03 +0200

    Amusement Park w/ Coin Flip Game
    just say الحمد الله and keep going homie

--- exercises/ft6/t6.js
+++ exercises/ft6/t6.js
@@ -9,3 +9,31 @@ if(hour > 6 && hour < 12 ) {
 } else {
     console.log(`Good Night ${name1}!`);
 }
+
+// Amusement Park
+
+let age = 6;
+let isHoliday = true
+if((age <= 6 || age >= 65) && isHoliday == false) {
+    console.log("Discount");
+}else if(isHoliday == true) {
+    console.log("We are off today.")
+}else {
+    console.log("No Discount for members between 6-65");
+}
+
+// Coin-flip game
+
+let random = Math.random();
+let compGuess = '';
+
+
+// if(random > 0.5) {
+//     compGuess = 'tail'    
+// }else {
+//     compGuess = 'head'
+// }
+
+// W/ ternary operations
+
+compGuess = random > 0.5 ? 'tail' : 'head';
