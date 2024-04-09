0a5059a1f43c6741c8f3e8bedd3407f8f26b28c6
MhmdSaker <professional24gamer7@gmail.com>
2024-04-09 15:07:57 +0200

    Added Advanced w/ Validations
    - AC when empty field
    - some shadows
    - fix field auto resize according to content

--- revision/calcfcc.js
+++ revision/calcfcc.js
@@ -1,19 +1,46 @@
 let calculation = localStorage.getItem('calculation') || '';
+let field = document.querySelector('.field');
+let clearBtn = document.querySelector('.clearBtn');
+let btns = document.querySelectorAll('.btn');
+
+
+
+display();
+
+
+function update(value) {
+    calculation += value;
+    display();
+}
+
+
+function eva() {              
+    calculation = eval(calculation);
+    display();
+    localStorage.setItem('calculation', calculation);
+
+
+}
+
+btns.forEach(function(element) {
+    element.addEventListener('click', function(){
+        clearBtn.innerHTML = 'C';
+    })
+})
+
+
+
+function clearDisplay() {
+    clearBtn.innerHTML = 'AC';
+    calculation = '';
+    display();
+    localStorage.setItem('calculation', calculation);
+}
 
 
 
 function display() {
-    document.querySelector('.field').innerHTML = calculation;
+    field.innerHTML = calculation;
 }
 
-function update(value) {
-    calculation += value;
-
-    display();
-}
-
-function eva() {
-    calculation = eval(calculation);
-    display();
-}
 
